8fcfb5e7959e16359f65616c52005ffd076bb298
Ahmedi7a <ahmedamralla73@email.com>
2025-03-20 13:22:15 +0300

    added three routes in car ctrl

--- controllers/car.js
+++ controllers/car.js
@@ -12,8 +12,43 @@ const router = express.Router();
 //anything bellow this the user has to sign in
 router.use(verifyToken);
 
+router.post('/', async (req, res) => {
+    try {
+        req.body.author = req.user._id;
+        const car = await Car.create(req.body);
+        car._doc.author = req.user;
+        res.status(201).json(car);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const cars = await Car.find({})
+            .populate('author')
+            .sort({ createdAt: 'desc' });
+        res.status(200).json(cars);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 
+router.delete('/:carId', async (req, res) => {
+    try {
+        const car = await Car.findById(req.params.carId);
 
+        if (!car.author.equals(req.user._id)) {
+            return res.status(403).send("You're not allowed to do that!");
+        }
+
+        const deletedcar = await Car.findByIdAndDelete(req.params.carId);
+        res.status(200).json(deletedcar);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 module.exports = router;
