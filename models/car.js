64aea240c7528e8b8d9ef691bfb70e3f2df2fd8d
Ahmedi7a <ahmedamralla73@email.com>
2025-03-20 10:58:02 +0300

    car modell

--- models/car.js
+++ models/car.js
@@ -22,5 +22,5 @@ const carSchema = new mongoose.Schema({
   reviews: [reviewSchema]
 }, { timestamps: true });
 
-const Car = mongoose.model('Car', hootSchema);
+const Car = mongoose.model('Car', carSchema);
 module.exports = Car;
