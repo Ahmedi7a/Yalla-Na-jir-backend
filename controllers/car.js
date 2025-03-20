1cefdbff19caad5fdac60ed69e32cf0a66f84141
Abbas <abboshuss@gmail.com>
2025-03-20 13:22:59 +0300

    created editing and getting a single car functions

--- controllers/car.js
+++ controllers/car.js
@@ -7,6 +7,31 @@ const router = express.Router();
 
 // ========= Protected Routes =========
 
+//get a car by id
+router.get('/:carId', async (req, res) => {
+    try {
+
+const car = await Car.findById(req.params.carId).populate([
+  'user',
+  'reviews.user',
+]);
+      res.status(200).json(car);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+
+  //edit car by id
+router.put('/:carId', async (req, res) => {
+    try {
+      const updatedCar = await Car.findByIdAndUpdate(req.params.carId, req.body, {
+        new: true,
+      });
+      res.status(200).json(updatedCar);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
 
 
 //anything bellow this the user has to sign in
